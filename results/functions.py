0de520f1eea51e00b5a04683559e3a9d0aa94887
Abhi chaurasia <abhi.chaurasia204@gmail.com>
2018-08-11 15:27:13 +0530

    add token generating function on each search

---
+++ results/functions.py
@@ -0,0 +1,74 @@
+import uuid
+import subprocess
+from django.conf import settings
+import os
+import json
+
+
+MEDIA_FOLDER = settings.MEDIA_ROOT
+
+def gen_token(video_quality,search_text):
+
+    folder_name = str(uuid.uuid4()) # folder_name = tokenName = SessionName
+
+    mkdirs(folder_name,search_text,video_quality)
+
+    generate_JSON_data(folder_name,search_text,video_quality) # generate JSON file in session-folder with fields = { titles, quality(=fetch quality)}
+
+    #Schedule Celery Task to delete the token folder(i.e. Session-folder after 24hours)
+
+    return folder_name
+
+
+def mkdirs(folder_name,text,quality): # text is search_text
+    folder_path = os.path.join(MEDIA_FOLDER,folder_name) # session folder path
+    stream_path = os.path.join(folder_path,'stream') # stream folder path
+    dl_path = os.path.join(folder_path,'dl') #download folder path
+    subprocess.run(['mkdir',folder_path,stream_path,dl_path])
+    formats = ["720p","480p","360p"]
+
+    main_dir = os.getcwd()
+    os.chdir(stream_path)
+
+    for format in formats:
+        quality_folder_path = str(os.path.join(stream_path,format))
+        subprocess.run(['mkdir',quality_folder_path])
+        if format == quality:
+            subprocess.run(['ytfs','-f',str(format),'-m','thumb',quality_folder_path]) # can add description META later
+            subprocess.run(['mkdir',os.path.join(quality_folder_path,text)]) #Takes time (But is required by webpage)
+        else:  #To be run by Celery for background execution
+            subprocess.run(['ytfs','-f',str(format),quality_folder_path])
+            subprocess.run(['mkdir',os.path.join(quality_folder_path,text)]) #Takes comparatively less time (But is required by webpage only when user changes streaming video quality)
+
+    os.chdir(main_dir)
+
+
+def generate_JSON_data(folder_name,text,quality): #folder_name -> session folder
+    token = folder_name #token == SessionName => generated folder on each search
+
+    files_list = subprocess.run(['ls',os.path.join(MEDIA_FOLDER,token,"stream",quality,text)],stdout=subprocess.PIPE).stdout.decode().split('\n')
+
+    print("files_list is :")
+    for x in files_list:
+        print(x)
+
+    titles = [os.path.splitext(x)[0] for x in files_list]
+    for title in titles:
+        if titles.count(title)>1:
+            titles.remove(title)    #Remove duplicate entries (present because of each video has an mp4 and thumb file )
+
+    data = {'token':token,"titles":titles}
+
+    data_file = open(os.path.join(MEDIA_FOLDER,token,'data.json'),'w+')
+    json.dump(data,data_file)
+    data_file.close()
+
+    pass
+
+
+
+
+
+
+
+folder_path = os.path.join(MEDIA_FOLDER,"sessionFolder",'blas') # makes session folder in project's media folder
--- results/views.py
+++ results/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render,redirect
+from . import functions
+
+from django.conf import settings
 
 # Create your views here.
 
 def results(request):
     if request.method == "POST":
         search_text = request.POST['search-text']
-        resp_quality = request.POST['search-quality']
-        return render(request,'results/results.html',{'resp_quality':resp_quality,'search_text':search_text})
+        resp_quality = request.POST['search-quality'].rstrip()
+##REGISTER [IP,search_text,Time] in Database Table
+        ip = ""
+        if "HTTP_X_REAL_IP" in request.META.keys():
+            ip = request.META["HTTP_X_REAL_IP"]
+        else:
+            ip = request.META["REMOTE_ADDR"]
+#################################################
+
+        token = functions.gen_token(resp_quality,search_text) ##For test Purpose
+
+        return render(request,'results/results.html',{'resp_quality':resp_quality,'search_text':search_text,'test':""})
 
     else:
         return redirect('homepage')
