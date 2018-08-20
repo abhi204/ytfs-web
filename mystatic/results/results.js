e9ac75ac28ab9220c4f43726f3f7f60e6e3d99e1
Abhi chaurasia <abhi.chaurasia204@gmail.com>
2018-08-20 19:01:32 +0530

    update page layout

--- mystatic/results/results.js
+++ mystatic/results/results.js
@@ -21,39 +21,13 @@ function loadPage(data) {
   titles.forEach(title => videoField(title));
 }
 
-
 function videoField(title) {
   let createdVideoField = createVideoField(title);
-  let listContainer = document.querySelector(".video-list");
-  listContainer.appendChild(createdVideoField)
+  let videoFieldsContainer = document.querySelector(".video-fields-list");
+  videoFieldsContainer.appendChild(createdVideoField)
 }
 
-//page is loaded and elements are setup
-
-let playerSource = document.querySelector(".video-js");
-playerSettings = {controls:true,
-  preload:"auto",
-  fluid:true,
-  autoplay:false,
-}
-let player = videojs(playerSource,playerSettings);
-
-function showVideo(title){
-  title.addEventListener("click",showEvent);
-}
-
-let videoTitles = Array.from(document.querySelectorAll(".title"));
-videoTitles.forEach(title => showVideo(title));
-
-function showEvent(e) {
-  let hideField = document.querySelector('.hidden-field-data')
-  if (hideField.classList.contains("hidden")){
-    hideField.classList.remove("hidden");
-  }
-  else{
-    hideField.classList.add("hidden");
-  }
-}
+//
 
 function createVideoField(title) {
   let encodedSearchText = encodeURIComponent(searchText)
@@ -61,25 +35,16 @@ function createVideoField(title) {
   let videoField = document.createElement("div");
   videoField.className = "video-field";
   videoField.innerHTML = `<div class="video-field-data">
-        <div class="show-data">
-          <img src="/media/${token}/stream/${resp_quality}/${encodedSearchText}/${encodedTitle}.jpg" id="video">
-          <div class="title">
-            <h6><a href="#video">${title}</a></h6>
-          </div>
+    <div class="show-data">
+      <img src="/media/${token}/stream/${resp_quality}/${encodedSearchText}/${encodedTitle}.jpg">
+      <div class="title">
+        <h6>${title}</h6>
+        <div class="video-options">
+          <a type="button" class="btn btn-dark" href="/media/${token}/stream/${resp_quality}/${encodedSearchText}/${encodedTitle}.mp4" target="_blank">Stream</a>
+          <a type="button" class="btn btn-danger" href="/media/${token}/stream/${resp_quality}/${encodedSearchText}/${encodedTitle}.mp4" download>Download</a>
         </div>
       </div>
-      <div class="hidden-field-data hidden">
-        <hr class="full-hr" >
-        <video class="video-js vjs-big-play-centered" poster="/media/${token}/stream/${resp_quality}/${searchText}/${title}.jpg" >
-          <source src="/media/${token}/stream/${resp_quality}/${encodedSearchText}/${encodedTitle}.mp4" type="video/mp4">
-          <p class="vjs-no-js"> To view this video please enable JavaScript, and consider upgrading to a web browser that
-            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-          </p>
-        </video>
-        <hr>
-        <div class="download-list">
-          <a class="btn btn-outline-danger" href="/media/${token}/stream/${resp_quality}/${searchText}/${title}.mp4" download>Download</a>
-        </div>
-      </div>`
+    </div>
+    <hr>`
   return videoField;
 }
