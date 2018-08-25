17f15d096d2e9059f26dab0b2a94233cbc1e9143
Abhi chaurasia <abhi.chaurasia204@gmail.com>
2018-08-26 03:04:42 +0530

    minor fix in settings.py

--- ytfsweb/settings.py
+++ ytfsweb/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 'django.contrib.messages',
 'django.contrib.staticfiles',
 'homepage.apps.HomepageConfig',
-'results.apps.ResultsConfig'
+'results.apps.ResultsConfig',
 'download_app.apps.DownloadAppConfig',
 ]
 
